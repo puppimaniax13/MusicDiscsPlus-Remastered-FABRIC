e0fcafac9fab05ff52dc1f3fb6addbac6550e35b
Becker Geiger <becker.gei21@outlook.com>
2022-02-27 00:16:29 -0500

    Fixing to match forge

--- src/main/java/net/pupp/musicdiscsplus/MusicDiscsPlus.java
+++ src/main/java/net/pupp/musicdiscsplus/MusicDiscsPlus.java
@@ -11,7 +11,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
 public class MusicDiscsPlus implements ModInitializer {
-	public static final String MOD_ID = "musicdiscsplus-pupp";
+	public static final String MOD_ID = "musicdiscsplus";
 	public static final String MOD_NAME = "MusicDiscsPlus";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
